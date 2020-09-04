675c60643180f203628c69be0c3290a3d3a8ca5f
Toshi <waykiyoharu@gmail.com>
2020-09-04 15:30:25 +0900

    日付の表示をsqlを修正し変更

--- routes/frege.js
+++ routes/frege.js
@@ -5,7 +5,7 @@ const connection = require('../mysqlConnection');//db接続読み取り
 
 
 router.get('/', (req, res) => {
-    connection.query('SELECT * FROM imgtest',(error,results)=>{
+    connection.query('SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM imgtest',(error,results)=>{
       res.render('frege.ejs',{items:results})
       console.log(results)
     })
