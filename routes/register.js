5e99cb9924a158d92bbb17c81af96f0692498b48
Toshi <waykiyoharu@gmail.com>
2020-09-05 13:52:32 +0900

    同一メールアドレスでの登録を不可に

--- routes/register.js
+++ routes/register.js
@@ -10,17 +10,26 @@ router.get('/',(req, res)=>{
    });
 });
 
-router.post('/',(req,res)=>{
+router.post('/', function(req, res) {
   var name = req.body.name;
   var email = req.body.email;
   var password = req.body.password;
   var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-  // console.log('こんそるこんそるこんそるこんそるこんそるこんそるこんそるこんそるこんそるこんそる');
-  // console.log(req.body);
-  connection.query('INSERT INTO user (name, email, password, created_at) VALUES (?, ?, ?, ?)',[name, email, password, createdAt],(error, results)=>{
-      res.redirect('/login');
+  var emailExistsQuery = 'SELECT * FROM user WHERE email = "' + email + '" LIMIT 1'; // 追加
+  var registerQuery = 'INSERT INTO user (name, email, password, created_at) VALUES ("' + name + '", ' + '"' + email + '", ' + '"' + password + '", ' + '"' + createdAt + '")'; // 変更
+  connection.query(emailExistsQuery, function(err, email) {
+    var emailExists = email.length;
+    if (emailExists) {
+      res.render('register.ejs', {
+        title: '新規会員登録',
+        emailExists: '既に登録されているメールアドレスです'
+      });
+    } else {
+      connection.query(registerQuery, function(err, rows) {
+        res.redirect('/login');
+      });
+    }
   });
-
 });
 
 module.exports = router;
