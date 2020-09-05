451be86a44ac3b0464bcf58228fc0006acc9bb98
Toshi <waykiyoharu@gmail.com>
2020-09-05 11:03:32 +0900

    失敗２！削除の外部化
    routes側で以下のようにすると削除が実行されなかった
    失敗
    outer.get('/:id',(req,res)=>{}
    ポイントはルーティングに「/:id」としているところ
    
    失敗例
    outer.get('/',(req,res)=>{}
    他のルーティング同様「/」のみにしていた

--- routes/diaryDelete.js
+++ routes/diaryDelete.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const connection = require('../mysqlConnection');//db接続読み取り
 
 //削除のルーティング
-router.get('/diaryDelete/:id',(req,res)=>{
+router.get('/:id',(req,res)=>{
     connection.query('DELETE FROM imgtest WHERE id = ?',[req.params.id],(error,results)=>{
       res.redirect('/frege')
     })
---
+++ routes/diaryUpdate.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const connection = require('../mysqlConnection');//db接続読み取り
+
+
+
+  module.exports = router;
