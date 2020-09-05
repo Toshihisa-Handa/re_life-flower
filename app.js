a5eb356b160c428ca4d5cfa2f7e176d7545ed012
Toshi <waykiyoharu@gmail.com>
2020-09-05 11:21:33 +0900

    編集、更新のルーティングを外部化
    やり方は削除と同じ
    編集と、更新でroutes内のファイルを分けること大事

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ var fregeRouter = require('./routes/frege');
 var diarysRouter = require('./routes/diarys');
 var diaryRouter = require('./routes/diary');
 var diaryDeleteRouter = require('./routes/diaryDelete');
+var diaryEditRouter = require('./routes/diaryEdit');
 var diaryUpdateRouter = require('./routes/diaryUpdate');
 var mypageRouter = require('./routes/mypage');
 var registerRouter = require('./routes/register');
@@ -50,27 +51,13 @@ app.use('/frege', fregeRouter);
 app.use('/diarys', diarysRouter);
 app.use('/diary', diaryRouter);
 app.use('/diaryDelete/', diaryDeleteRouter);
-app.use('/diaryUpdate/:id', diaryUpdateRouter);
+app.use('/diaryEdit/', diaryEditRouter);
+app.use('/diaryUpdate/', diaryUpdateRouter);
 app.use('/mypage', mypageRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/fileup', fileupRouter);
 
-
-//日記編集画面へのルーティング
- app.get('/diaryEdit/:id',(req, res)=>{
-   connection.query('SELECT * FROM imgtest WHERE id = ?',[req.params.id],(error,result)=>{
-     res.render('diaryEdit.ejs',{item:result[0]});
-     console.log(result[0])
-   })
- })
-
- app.post('/diaryUpdate/:id',(req,res)=>{
-  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-	connection.query('UPDATE imgtest SET title = ?, tag = ?, text = ? , created_at = ? WHERE id = ?',[req.body.title, req.body.tag, req.body.text, createdAt, req.params.id],function (error, result) {  
-		res.redirect('/frege');
-  	});
-});
 
 
 
---
+++ routes/diaryEdit.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const connection = require('../mysqlConnection');//db接続読み取り
+
+
+//日記編集画面へのルーティング
+router.get('/:id',(req, res)=>{
+    connection.query('SELECT * FROM imgtest WHERE id = ?',[req.params.id],(error,result)=>{
+      res.render('diaryEdit.ejs',{item:result[0]});
+      console.log(result[0])
+    })
+  })
+ 
+  module.exports = router;
