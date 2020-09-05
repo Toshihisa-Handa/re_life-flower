6698da58914d79d24d9616c9963e45f12fdee0a1
Toshi <waykiyoharu@gmail.com>
2020-09-05 13:02:59 +0900

    ユーザー登録画面の登録内容をmysqlに反映

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../mysqlConnection');//db接続読み取り
+const moment = require('moment');//日付取得用パッケージ読み込み
+
 
 router.get('/',(req, res)=>{
-    // res.renderで指定ファイルの画面表示させる
-    res.render('register.ejs');
+   res.render('register.ejs', {
+       title:'新規会員登録'
+   });
+});
+
+router.post('/',(req,res)=>{
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  // console.log('こんそるこんそるこんそるこんそるこんそるこんそるこんそるこんそるこんそるこんそる');
+  // console.log(req.body);
+  connection.query('INSERT INTO user (name, email, password, created_at) VALUES (?, ?, ?, ?)',[name, email, password, createdAt],(error, results)=>{
+      res.redirect('/login');
+  });
+
 });
 
 module.exports = router;
