4266fea6d2fe7cd79d87f2685214edce9d0824c7
Toshi <waykiyoharu@gmail.com>
2020-09-10 15:30:16 +0900

    編集画面へのルーティング実装

--- routes/myprofileEdit.js
+++ routes/myprofileEdit.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();
 const connection = require('../mysqlConnection');//db接続読み取り
 
-router.get('/',(req, res)=>{
-  // res.renderで指定ファイルの画面表示させる
-  res.render('myprofileEdit.ejs');
-
-
-  
-});
+router.get('/:id',(req, res)=>{
+  connection.query('SELECT * FROM shop WHERE id = ?',[req.params.id],(error,result)=>{
+    res.render('myprofileEdit.ejs',{item:result[0]});
+    console.log(result[0])
+  })
+})
 
 module.exports = router;
