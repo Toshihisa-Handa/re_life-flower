ff1ddd36288df45b25bd0941b1e0b359c7b305a0
Toshi <waykiyoharu@gmail.com>
2020-09-10 15:49:33 +0900

    マイページの更新のsqlを変数化

--- routes/myprofileUpdate.js
+++ routes/myprofileUpdate.js
@@ -8,7 +8,8 @@ const moment = require('moment');//日付取得用パッケージ読み込み
 
   router.post('/:id',(req,res)=>{
    var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-     connection.query('UPDATE shop SET name = ?, title = ?, created_at = ? WHERE id = ?',[req.body.name, req.body.title, createdAt, req.params.id],function (error, result) {  
+   var sql = 'UPDATE shop SET name = ?, title = ?, created_at = ? WHERE id = ?'
+     connection.query(sql,[req.body.name, req.body.title, createdAt, req.params.id],function (error, result) {  
          res.redirect('/myprofile');
        });
  });
