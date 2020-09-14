a85cb7db4e23896bbfdbb3c37dfe84a971440cbe
Toshi <waykiyoharu@gmail.com>
2020-09-14 21:26:37 +0900

    forEachを使うと複数取得可能だが、今のsqlの書き方では重複が出る

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,8 @@ router.use(express.static(path.join(__dirname, 'public')))
 router.get('/:id',(req, res)=>{
     // connection.query('SELECT * FROM shop WHERE id = ?',[req.params.id],(error,result)=>{
     connection.query('SELECT D.image AS dimg, F.image AS fimg, S.id, S.user_id, S.name, S.title, S.account_name, S.web, S.email, S.tell, S.open, S.close, S.holiday, S.location, S.map, S.account_img, S.shop_img, S.img1, S.img2, S.message, S.comment, S.created_at FROM shop S LEFT JOIN diary D ON S.user_id = D.user_id LEFT JOIN flower F ON S.user_id = F.user_id WHERE S.id = ?',[req.params.id],(error,result)=>{
-      res.render('shop.ejs',{item:result[0]});
-      console.log(result[0])
+      res.render('shop.ejs',{item:result});
+      console.log(result)
     })
   })
  
