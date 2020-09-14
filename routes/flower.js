e203b6effe257f796291b9271f1525639f8c627a
Toshi <waykiyoharu@gmail.com>
2020-09-14 16:40:25 +0900

    日記、お花詳細ページにshopテーブルのnameを取得出来るよう修正をした

--- routes/flower.js
+++ routes/flower.js
@@ -6,9 +6,9 @@ router.use(express.static('public'));
 router.use(express.static(path.join(__dirname, 'public')))
 
 router.get('/:id',(req, res)=>{
-    connection.query('SELECT * FROM flower WHERE id = ?',[req.params.id],(error,result)=>{
+    connection.query('SELECT S.name AS shopname, F.id, F.user_id, F.image, F.name, F.price, F.feature, F.text FROM flower AS F LEFT JOIN shop S ON F.user_id = S.user_id WHERE F.id = ?',[req.params.id],(error,result)=>{
       res.render('flower.ejs',{item:result[0]});
-      console.log(result[0])
+      console.log('testttttttttt')
     })
   })
  
--- routes/flowers.js
+++ routes/flowers.js
@@ -4,7 +4,7 @@ const connection = require('../mysqlConnection');//db接続読み取り
 
 
 router.get('/', (req, res) => {
-    var sql = 'SELECT F.id, F.user_id, F.name, F.image, F.price, F.feature, F.tag, F.text,ifnull(U.name, \'名無し\') AS uname, DATE_FORMAT(F.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM flower F LEFT OUTER JOIN user U ON F.user_id = U.user_id ORDER BY F.created_at DESC'; 
+    var sql = 'SELECT S.name AS shopname, F.id, F.user_id, F.name, F.image, F.price, F.feature, F.tag, F.text,ifnull(U.name, \'名無し\') AS uname, DATE_FORMAT(F.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM flower F LEFT OUTER JOIN user U ON F.user_id = U.user_id JOIN shop S ON F.user_id = S.user_id ORDER BY F.created_at DESC'; 
     connection.query(sql,(error,results)=>{
         res.render('flowers.ejs',{items:results})
         console.log(results)
