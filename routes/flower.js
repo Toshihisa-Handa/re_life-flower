5b3ea204500223b4aa69c9bfcaf016263f1390a5
Toshi <waykiyoharu@gmail.com>
2020-09-14 14:20:20 +0900

    お花詳細ページを作成（sqlのjoin未完了）

--- routes/flower.js
+++ routes/flower.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../mysqlConnection');//db接続読み取り
+const path = require('path')
+router.use(express.static('public'));
+router.use(express.static(path.join(__dirname, 'public')))
 
-router.get('/',(req, res)=>{
-    // res.renderで指定ファイルの画面表示させる
-    res.render('flower.ejs');
-});
-
-module.exports = router;
+router.get('/:id',(req, res)=>{
+    connection.query('SELECT * FROM flower WHERE id = ?',[req.params.id],(error,result)=>{
+      res.render('flower.ejs',{item:result[0]});
+      console.log(result[0])
+    })
+  })
+ 
+  module.exports = router;
