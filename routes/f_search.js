e1171ace8061c2589fc2c14ae6bcac478e2c241a
Toshi <waykiyoharu@gmail.com>
2020-09-15 21:07:14 +0900

    色々曖昧検索を可能にした

--- routes/f_search.js
+++ routes/f_search.js
@@ -5,7 +5,7 @@ const connection = require('../mysqlConnection');//db接続読み取り
 
 router.get('/', (req, res) => {
     let kensaku = req.query.kensaku;
-    var sql = "SELECT S.name AS shopname, S.location AS basyo, F.id, F.user_id, F.name, F.image, F.price, F.feature, F.tag, F.text,ifnull(U.name, \'名無し\') AS uname, DATE_FORMAT(F.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM flower F LEFT OUTER JOIN user U ON F.user_id = U.user_id JOIN shop S ON F.user_id = S.user_id WHERE S.name LIKE '%"+kensaku+"%' OR S.location LIKE '%"+kensaku+"%' ORDER BY F.created_at DESC"; 
+    var sql = "SELECT S.name AS shopname, S.location AS basyo, F.id, F.user_id, F.name, F.image, F.price, F.feature, F.tag, F.text,ifnull(U.name, \'名無し\') AS uname, DATE_FORMAT(F.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM flower F LEFT OUTER JOIN user U ON F.user_id = U.user_id JOIN shop S ON F.user_id = S.user_id WHERE S.name LIKE '%"+kensaku+"%' OR S.location LIKE '%"+kensaku+"%' OR F.name LIKE '%"+kensaku+"%' OR F.feature LIKE '%"+kensaku+"%' OR F.tag LIKE '%"+kensaku+"%' OR F.text LIKE '%"+kensaku+"%' ORDER BY F.created_at DESC"; 
 
     connection.query(sql,(error,results)=>{
         res.render('flowers.ejs',{items:results})
