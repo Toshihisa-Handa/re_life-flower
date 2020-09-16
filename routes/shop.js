0e7bec1317190b08585966bf477257bd7bbad980
Toshi <waykiyoharu@gmail.com>
2020-09-16 16:34:24 +0900

    店舗の登録の花と日記一覧を取得可能にした※user_idの取得に苦労した　sqlの入れ子についての理解も深まった
    flowerテーブルとdiaryテーブルのuser_idを
    console.log(result[0].user_id)
    で取得できることに気づくのに苦労した
    sqlは入れ子にするとselect文で複数取得出来ることがわかった！

--- routes/shop.js
+++ routes/shop.js
@@ -7,18 +7,34 @@ router.use(express.static(path.join(__dirname, 'public')))
 
 //日記編集画面へのルーティング
 router.get('/:id',(req, res)=>{
-    var userId = req.session.user_id? req.session.user_id: 0;
-    var flowers = 'SELECT F.id, F.user_id, F.name, F.image, F.price, F.feature, F.tag, F.text,ifnull(U.name, \'名無し\') AS uname, DATE_FORMAT(F.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM flower F LEFT OUTER JOIN user U ON F.user_id = U.user_id WHERE F.user_id = ' + userId + '  ORDER BY F.created_at DESC'; // 変更
-    var diarys = 'SELECT D.id, D.user_id, D.title, D.image, D.tag, D.text,ifnull(U.name, \'名無し\') AS name, DATE_FORMAT(D.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM diary D LEFT OUTER JOIN user U ON D.user_id = U.user_id WHERE D.user_id = ' + userId + '  ORDER BY D.created_at DESC'; // 変更
-connection.query(flowers,(error,flower)=>{
-  connection.query(diarys,(error,diary)=>{
+  // console.log(req.params.id)// ページのid
+    // var uid = req.params.id;//これはshopテーブルのid
+    var shop = 'SELECT * FROM shop'
+   
+    var sql = 'SELECT D.image AS dimg, F.image AS fimg, S.id, S.user_id, S.name, S.title, S.account_name, S.web, S.email, S.tell, S.open, S.close, S.holiday, S.location, S.map, S.account_img, S.shop_img, S.img1, S.img2, S.message, S.comment, S.created_at FROM shop S LEFT JOIN diary D ON S.user_id = D.user_id LEFT JOIN flower F ON S.user_id = F.user_id WHERE S.id = ?';
   // connection.query('SELECT * FROM shop WHERE id = ?',[req.params.id],(error,result)=>{
-    connection.query('SELECT D.image AS dimg, F.image AS fimg, S.id, S.user_id, S.name, S.title, S.account_name, S.web, S.email, S.tell, S.open, S.close, S.holiday, S.location, S.map, S.account_img, S.shop_img, S.img1, S.img2, S.message, S.comment, S.created_at FROM shop S LEFT JOIN diary D ON S.user_id = D.user_id LEFT JOIN flower F ON S.user_id = F.user_id WHERE S.id = ?',[req.params.id],(error,result)=>{
-      res.render('shop.ejs',{item:result, fitem:flower, ditem:diary});//renderではなく、resultを別の配列に入れておき、
-      console.log(result)
+    connection.query(sql,[req.params.id],(error,result)=>{
+        let uid = result[0].user_id;
+        var flowers = 'SELECT * FROM flower WHERE user_id = '+ uid +''
+        var diarys = 'SELECT * FROM diary WHERE user_id = '+ uid +''
+
+      connection.query(flowers,(error,flower)=>{
+        connection.query(diarys,(error,diary)=>{
+          res.render('shop.ejs',{fitem:flower, ditem:diary, item:result});//renderではなく、resultを別の配列に入れておき、
+          // console.log(result)
+          console.log('いいいいいいいいいいいいいいいいいいい')
+          console.log(uid)
+          console.log(flower)
+          console.log(diary)
+          console.log(result[0].user_id)
+          // console.log(result)
+
+        })
+
+
+      })
+    
     })
-  })
-})
   
     //renderで返す（保留）レンダーだと情報が空
     //connection.queryはおそらく非同期　その処理が終わる前に入れ子にしてやるパターンもありかもしれない。
